6caa512d1adf3903d9028b017d641e6b89d8a7c4
Afrida-Islam <afrida0627@gmail.com>
2026-01-08 13:22:30 +0600

    solved problem 2 (count vowels)

--- problem.js
+++ problem.js
@@ -9,3 +9,25 @@ function reverseString(str) {
   return reversed;
 }
 console.log(reverseString("hello"));
+
+// Problem 2: Count Vowels in a String
+
+function countVowels(str) {
+  let count = 0;
+  let lowerStr = str.toLowerCase();
+  for (let i = 0; i < lowerStr.length; i++) {
+    let char = lowerStr[i];
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      count++;
+    }
+  }
+
+  return count;
+}
+console.log(countVowels("programming"));
