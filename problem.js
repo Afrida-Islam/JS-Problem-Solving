952070bdbedc45eeb7d905fb40f8f5a0a7f9e656
Afrida-Islam <afrida0627@gmail.com>
2026-01-08 14:11:45 +0600

    solved problem 8 (Capitalize First Letter of Each Word)

--- problem.js
+++ problem.js
@@ -88,3 +88,15 @@ function getEvenNumbers(arr) {
   return evens;
 }
 console.log(getEvenNumbers([1, 2, 3, 4, 5, 6]));
+
+// Problem 8: Capitalize First Letter of Each Word
+function capitalizeWords(sentence) {
+  let words = sentence.split(" ");
+
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+  }
+
+  return words.join(" ");
+}
+console.log(capitalizeWords("hello world"));
